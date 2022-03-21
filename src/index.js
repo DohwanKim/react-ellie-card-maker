e82fc65b41bbc2d364e48f5011ee37273fafcf30
DohwanKim <hwan@spadecompany.kr>
2022-03-21 17:36:50 +0900

    Feat: Cloudinary private setting

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from 'app';
 import { firebaseApp } from 'service/firebase';
 import AuthService from 'service/auth_service';
 import ImageUploader from 'service/image_uploader';
-import ImageFileInput from './components/image_file_input/image_file_input';
+import ImageFileInput from 'components/image_file_input/image_file_input';
 
 const authService = new AuthService(firebaseApp);
-const imageUploader = new ImageUploader();
+const imageUploader = new ImageUploader(
+  process.env.REACT_APP_CLOUDINARY_NAME,
+  process.env.REACT_APP_CLOUDINARY_API_KEY,
+);
 const FileInput = props => <ImageFileInput {...props} imageUploader={imageUploader} />;
 
 ReactDOM.render(
--- src/service/image_uploader.js
+++ src/service/image_uploader.js
@@ -1,15 +1,22 @@
 import axios from 'axios';
 
 class ImageUploader {
-  _url = 'https://api.cloudinary.com/v1_1/demo/image/upload';
+  _uploadUrl;
+  _apiKey;
+
+  constructor(cloudinaryName, cloudinaryApiKey) {
+    this._uploadUrl = `https://api.cloudinary.com/v1_1/${cloudinaryName}/image/upload`;
+    this._apiKey = cloudinaryApiKey;
+  }
 
   async upload(file) {
     const formData = new FormData();
 
     formData.append('file', file);
-    formData.append('upload_preset', 'docs_upload_example_us_preset');
+    formData.append('upload_preset', 'ellie-card-maker');
+    formData.append('api_key', this._apiKey);
 
-    return await axios.post(this._url, formData);
+    return await axios.post(this._uploadUrl, formData);
   }
 }
 
