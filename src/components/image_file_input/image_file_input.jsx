27568c4fffa80bfa7e3c911d55c5a0fdd923b10f
DohwanKim <hwan@spadecompany.kr>
2022-03-21 18:00:12 +0900

    Feat: Image upload loading spinner

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,13 +1,15 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from 'components/image_file_input/image_file_input.module.css';
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const imageInputRef = useRef();
   const onImageInput = e => {
     e.preventDefault();
     imageInputRef.current.click();
   };
   const onChangeImageInput = async e => {
+    setIsLoading(true);
     const uploaded = await imageUploader.upload(e.currentTarget.files[0]);
 
     if (uploaded) {
@@ -16,13 +18,20 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
         url: uploaded.data.url,
       });
     }
+    setIsLoading(false);
   };
 
   return (
     <>
-      <button type="button" onClick={onImageInput}>
-        {name || 'No file'}
-      </button>
+      {!isLoading ? (
+        <button type="button" onClick={onImageInput}>
+          {name || 'No file'}
+        </button>
+      ) : (
+        <div className={styles.loadingContainer}>
+          <span className={styles.loading} />
+        </div>
+      )}
       <input
         ref={imageInputRef}
         className={styles.fileInput}
